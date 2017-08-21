9dbc7d39b41cfcaa252708d52b6721792adcf9ad
Elliot Waite <elliotwaite@gmail.com>
2017-08-21 14:41:13 -0700

    Fix `plugin_data` field assignment.
    Since `plugin_data` is not a repeated field, it does not have the add() method, and field assignment should instead be done directly.

--- greeter_plugin/greeter_summary.py
+++ greeter_plugin/greeter_summary.py
@@ -55,7 +55,8 @@ def op(name,
   summary_metadata = tf.SummaryMetadata()
   # We could put additional metadata other than the PLUGIN_NAME,
   # but we don't need any metadata for this simple example.
-  summary_metadata.plugin_data.add(plugin_name=PLUGIN_NAME, content="")
+  summary_metadata.plugin_data.plugin_name = PLUGIN_NAME
+  summary_metadata.plugin_data.content = ""
   message = tf.string_join(['Hello, ', guest, '!'])
   # Return a summary op that is properly configured.
   return tf.summary.tensor_summary(
@@ -85,9 +86,8 @@ def pb(tag, guest, display_name=None, description=None):
                                         summary_description=description)
   # We have no metadata to store, but we do need to add a plugin_data entry
   # so that we know this summary is associated with the greeter plugin.
-  metadata_content = '{}'
-  summary_metadata.plugin_data.add(plugin_name=PLUGIN_NAME,
-                                   content=metadata_content)
+  summary_metadata.plugin_data.plugin_name = PLUGIN_NAME
+  summary_metadata.plugin_data.content = '{}'
 
   summary = tf.Summary()
   summary.value.add(tag=tag,
